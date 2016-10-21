df96b98ada891555a41ae605a24f34b2e1518b6f
mapengtang <mapengtang@enn.cn>
2016-10-21 13:46:20 +0800

    生成文件

---
+++ app/src/main/java/com/ma/generateandroiddiffdpi/GenerateActivity.java
@@ -0,0 +1,13 @@
+package com.ma.generateandroiddiffdpi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GenerateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generate);
+    }
+}
